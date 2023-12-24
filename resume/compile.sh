5eaf8c055c772142aeddd28760d97dc8ff07aa27
Ben Schwartz <btschwartz12@gmail.com>
2023-12-24 08:20:27 +0000

    Updated

--- resume/compile.sh
+++ resume/compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Compiling resume.tex to resume.pdf"
+# make it so the environment variables are available
 git pull
 pdflatex --output-directory=pdflatex-output resume.tex
 cp pdflatex-output/resume.pdf resume.pdf	
